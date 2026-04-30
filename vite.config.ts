4fdfe4f00a557ee01c55c0694eb04b6de9bdee21
Daniil <krupnov025@gmail.com>
2026-04-30 11:44:21 +0300

    Setup GitHub Actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/inner/',
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://wmnmnmnpqnfnlmtegltz.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('твой_анонимный_ключ'),
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
