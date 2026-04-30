f2264241775ead38a22dd22736ed43f90049019a
Daniil <krupnov025@gmail.com>
2026-04-30 21:05:01 +0300

    fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/inner/',
-  define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://wmnmnmnpqnfnlmtegltz.supabase.co'),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('твой_анонимный_ключ'),
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
